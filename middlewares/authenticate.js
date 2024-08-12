f89649d945ac6ee68e16473d8f1e6a8ce01b7d6d
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-08-12 19:52:00 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const {SECRET_KEY} = process.env;
 const authenticate = async (req, res, next) => {
     
     const {authorization = ""} = req.headers;
-    console.log(authorization);
+    console.log("authorization=",authorization);
     const [bearer, token] = authorization.split(" ");
     
     if (bearer !== "Bearer") {

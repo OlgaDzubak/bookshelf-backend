8b9a1e322bd90a0655a5bc29c45c1930ed159cc2
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-04 23:25:01 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -40,8 +40,8 @@ const {SECRET_KEY, BASE_URL} = process.env;
     const {_id, name: currentUserName} = req.user;                                                  //забираємо поточне ім'я юзера
     const {name} = req.body;                                                                        //забираємо нове ім'я юзера
 
-    console.log("name=", newName);
-    
+    console.log("name=", name);
+
     if (!name) { newUserName = currentUserName}
     else { newUserName = name};
     

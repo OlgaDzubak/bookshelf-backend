7a463b100b4d11eaba42524e0c08c6d72118ab50
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-07 21:57:10 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // -------------------------------------------------------------------------------------------------------------
 
 router.get('/current', authenticate, ctrl.getCurrent);
-router.patch('/update', authenticate, validateBody(schemas.updateSchema) , ctrl.updateUser); //upload.single("avatar"),
+router.patch('/update', authenticate, ctrl.updateUser); //upload.single("avatar"),
 router.post('/subscribe',authenticate, ctrl.subscribe);
 
 // -------------------------------------------------------------------------------------------------------------

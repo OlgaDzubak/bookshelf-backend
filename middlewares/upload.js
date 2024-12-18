66ad0b8e67146c64c904ea8c8c150a303ee80548
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-18 15:54:28 +0200

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const {httpError} = require('../helpers');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
@@ -11,27 +12,32 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
-    
-    console.log("я в upload new CloudinaryStorage");
-    
-    let folder;
 
-    if (file.fieldname === 'avatar') {
-      console.log("file.fieldname === 'avatar'");
-      folder = 'avatars';
-    }
-    
-    console.log("я перед return CloudinaryStorage");
-    return {
-      folder: folder,
-      allowed_formats: ["jpg", "png"],
-      public_id: file.originalname, 
-      transformation: [
-        { height: 250, crop: "scale" },
-        { height: 250, crop: "scale" },
-      ],
-    };
-  },
+    try {
+      console.log("я в upload new CloudinaryStorage");
+      let folder;
+
+      if (file.fieldname === 'avatar') {
+        console.log("file.fieldname === 'avatar'");
+        folder = 'avatars';
+      }
+      
+      console.log("я перед return CloudinaryStorage");
+      return {
+        folder: folder,
+        allowed_formats: ["jpg", "png"],
+        public_id: file.originalname, 
+        transformation: [
+                          { height: 250, crop: "scale" },
+                          { height: 250, crop: "scale" },
+                        ],
+        };
+    }catch(error){
+       console.log(error);
+       if (error.message = "") {
+         next(httpError(500, "Cloudinary uploading error. Wrong file format!"));
+       }
+    }},
 });
 
 const upload = multer({ storage });

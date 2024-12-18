d437aeaa7e3fd7a7825c1d1c9038a6a75dc43562
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-19 00:31:06 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ctrl = require('../controllers/users');
-const {authenticate, upload}  = require("../middlewares");
+const {authenticate, validateBody, upload}  = require("../middlewares");
+const {schemas} = require("../db/models/user");
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ const router = express.Router();
 
 router.get('/current', authenticate, ctrl.getCurrent);
 router.post('/subscribe', authenticate, ctrl.subscribe);
-router.patch('/update', authenticate, upload.single('avatar'), ctrl.updateUser); //validateBody(schemas.updateSchema), upload.single('avatar'), 
+router.patch('/update', authenticate, validateBody(schemas.updateSchema), upload.single('avatar'), ctrl.updateUser); //validateBody(schemas.updateSchema), upload.single('avatar'), 
 
 
 // -------------------------------------------------------------------------------------------------------------

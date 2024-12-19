ea26ae62b606cff61fe92db4088ed5b26c5671cd
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-19 13:45:59 +0200

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -36,6 +36,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage,
                         fileFilter: (req, file, cb)=>{
                             if (file.mimetype !== 'image/png') {
+                              console.log("file = ", file);
                               req.fileValidationError = 'goes wrong on the mimetype';
                               return cb(null, false, new Error('goes wrong on the mimetype'));
                              }

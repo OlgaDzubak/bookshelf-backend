540c6a0ba95eb40e597cdd3f044b70c12547065e
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-12-22 17:43:10 +0200

    Update user.js

--- db/models/user.js
+++ db/models/user.js
@@ -14,7 +14,6 @@ const now = new Date();
             //дані з форми регістрації
             name: {
                 type: String,
-                required: [true, 'Set name for user'],
                 default: "User",
                 match: nameRegExp,
                 minlength: 2,
